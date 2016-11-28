8a887ca123d1a55ec9b32d7db7a19f5f297a4571
Jagdish Panchal <jagdish.panchal@serpentcs.com>
2016-11-28 20:07:53 +0530

    [ADD] Added customer po field in invoice serach view and added status field in purchase register report

--- galaxy_purchase/wizard/purchase_register_wizard.py
+++ galaxy_purchase/wizard/purchase_register_wizard.py
@@ -56,7 +56,7 @@ class purchase_register_report(models.Model):
     amt = fields.Float('Amount',readonly=True)
     amt_due = fields.Float('Amount Due',readonly=True)
     state = fields.Char('Status',readonly=True)
-#     received = fields.Boolean('Received',readonly=True)
+    received = fields.Boolean('Received',readonly=True)
     _order ="pdate"
     
     def init(self, cr):
@@ -64,7 +64,9 @@ class purchase_register_report(models.Model):
         cr.execute("""create or replace view purchase_register_report as
             (SELECT in_inv.id,in_inv.number as pur_order,in_inv.date_invoice as pdate, 
                 in_inv.amount_total as amt,in_inv.state as state,rp.name as sup_name,
-                in_inv.supplier_invoice_number as sup_inv,in_inv.residual as amt_due
+                in_inv.supplier_invoice_number as sup_inv,in_inv.residual as amt_due,
+                in_inv.customer_po as received
+                
                 FROM account_invoice in_inv
                 INNER JOIN res_partner rp ON rp.id = in_inv.partner_id
                 where in_inv.type = 'in_invoice'
